7144f76b760ec3478fe984ff5180318f00a4e4e2
Piyush <achyutapiyush@gmail.com>
2014-04-18 12:28:28 +0530

    Improving catalog view

--- src/librehatti/librehatti/catalog/views.py
+++ src/librehatti/librehatti/catalog/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from librehatti.catalog.models import *
 
 def index(request):
-	return render(request, 'catalog.html')
+	# we have to show all products
+	error = {}
+
+	# Collect all the categories first
+	categorylist = category.objects.all()
+	if categorylist.count() == 0:
+		nocategory = True;
+
+	return render(request, 'catalog.html', {'nocategory': nocategory})
 	pass
