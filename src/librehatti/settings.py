174d8c6d362292264712f468c6d0f0f66ca11e6f
Piyush <achyutapiyush@gmail.com>
2014-04-25 22:22:31 +0530

    Adapting Change in directory structure

--- src/librehatti/settings.py
+++ src/librehatti/settings.py
@@ -109,7 +109,7 @@ ROOT_URLCONF = 'librehatti.urls'
 WSGI_APPLICATION = 'librehatti.wsgi.application'
 
 TEMPLATE_DIRS = (
-    '../templates',
+    'templates/',
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
