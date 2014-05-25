67226b13b527191372b41a992854be84470a171e
Piyush <achyutapiyush@gmail.com>
2014-05-25 15:22:06 +0530

    Moving things

--- src/librehatti/catalog/admin.py
+++ src/librehatti/catalog/admin.py
@@ -6,11 +6,6 @@ admin.autodiscover()
 admin.site.register(category)
 admin.site.register(attributes)
 admin.site.register(catalog)
-admin.site.register(admin_organisations)
-admin.site.register(address)
-admin.site.register(organisation_type)
-admin.site.register(customer)
-admin.site.unregister(User)
 
 class CatalogInline(admin.TabularInline):
 	model = catalog
@@ -21,20 +16,6 @@ class ProductAdmin(admin.ModelAdmin):
     fields = ['name','category','price','organisation']
     inlines = [CatalogInline] 
 
-class AddressInline(admin.StackedInline):
-     model = address
-
-class CustomerInline(admin.StackedInline):
-	model = customer
-
-class customUserAdd(UserAdmin):
-    add_fieldsets = (
-        ('Add Customer', {
-            'classes': ('wide',),
-            'fields': ('username', 'email','first_name','last_name', 'password1', 'password2')}
-        ),
-    )
-    inlines = [CustomerInline]
 
 class PurchasedItemInline(admin.StackedInline):
 	model = purchased_item
@@ -46,6 +27,5 @@ class PurchaseOrderAdmin(admin.ModelAdmin):
 	
 
 admin.site.register(purchase_order, PurchaseOrderAdmin)
-admin.site.register(User,customUserAdd)
 admin.site.register(product, ProductAdmin) 
 
