f84c1c189930f877fcc6bf47de2b28db62fdae83
Gunpreet Ahuja <gunpreetahuja0528@gmail.com>
2014-06-05 20:28:05 +0530

    Comments removed and indentation corrected

--- src/useraccounts/admin.py
+++ src/useraccounts/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from useraccounts.models import *
 from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
 admin.site.register(admin_organisations)
 admin.site.register(address)
 admin.site.register(organisation_type)
@@ -13,13 +12,14 @@ class AddressInline(admin.StackedInline):
      model = address
 
 class CustomerInline(admin.StackedInline):
-	model = customer
+     model = customer
 
 class customUserAdd(UserAdmin):
     add_fieldsets = (
         ('Add Customer', {
             'classes': ('wide',),
-            'fields': ('username', 'email','first_name','last_name', 'password1', 'password2')}
+            'fields': ('username', 'email', 'first_name', 'last_name', 
+            'password1', 'password2')}
         ),
     )
     inlines = [CustomerInline]
