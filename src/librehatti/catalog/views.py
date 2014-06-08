0560490fb8869feb8c711d42913973bc3c123164
Gunpreet Ahuja <gunpreetahuja0528@gmail.com>
2014-06-08 20:57:05 +0530

    4 spaces

--- src/librehatti/catalog/views.py
+++ src/librehatti/catalog/views.py
@@ -5,35 +5,34 @@ from librehatti.catalog.forms import *
 
 
 def index(request):
-        error = {}
-        categorylist = Category.objects.all()
+    error = {}
+    categorylist = Category.objects.all()
 
-        if categorylist.count() == 0:
-                nocategory = True
-                return render(request, 'catalog.html', {'nocategory': 
+    if categorylist.count() == 0:
+        nocategory = True
+        return render(request, 'catalog.html', {'nocategory': 
                        nocategory})
-        productlist = Product.objects.all();
+    productlist = Product.objects.all();
 
-        if productlist.count() == 0:
-                noproduct = True
-                return render(request, 'catalog.html', {'noproduct': noproduct})
+    if productlist.count() == 0:
+        noproduct = True
+        return render(request, 'catalog.html', {'noproduct': noproduct})
 
-        return render(request,'catalog.html', {'productlist': productlist, 
+    return render(request,'catalog.html', {'productlist': productlist, 
                'categorylist': categorylist})
 
-        pass
+    pass
 
 
 def add_categories(request):
 
-        if request.method == 'POST' :
-                form = addCategory(request.POST)
-                if form.is_valid():
-                        return HttpResponseRedirec('/')
-        else:
-                form = addCategory()
-
-        return render(request, 'addCategory.html', {
-                'form':form,
-        })
+    if request.method == 'POST' :
+        form = addCategory(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirec('/')
+    else:
+        form = addCategory()
+    return render(request, 'addCategory.html', {
+            'form':form,
+    })
 
