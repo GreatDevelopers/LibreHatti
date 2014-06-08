895917a0cb5e9a10c437d1ed648fd50daed0345d
Jaskaran <lamba.jaskaran@gmail.com>
2014-06-09 00:20:32 +0530

    import useraccounts and user

--- src/librehatti/catalog/models.py
+++ src/librehatti/catalog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.forms import ModelForm
-
+import useraccounts
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100)
