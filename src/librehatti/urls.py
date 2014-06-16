05eb195c0c415b70a37be358e2311e2bd79dbdff
ashu <like-a-boss>
2014-06-16 19:45:51 +0530

    General Report

--- src/librehatti/urls.py
+++ src/librehatti/urls.py
@@ -2,12 +2,13 @@ from django.conf.urls import patterns, include, url
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-
+from librehatti.report.views import report
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
         url(r'^$', 'librehatti.catalog.views.index'),
+        url(r'^report/', 'librehatti.report.views.report'),
         url(r'^catalog/', include('librehatti.catalog.urls')),
         url(r'^useraccounts/', include('useraccounts.urls')),
         url(r'^admin/', include(admin.site.urls)),
