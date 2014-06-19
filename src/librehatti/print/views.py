37582ac35d27389aab667a750fd7201963d95732
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-06-19 15:38:45 +0530

    views modified and bill.html attached

--- src/librehatti/print/views.py
+++ src/librehatti/print/views.py
@@ -1,25 +1,23 @@
+ # Create your views here.
+
 from django.shortcuts import render
 from librehatti.catalog.models import *
-
 import datetime
 from django.db.models import Sum
 
 def bill(request):
     
-    pi = PurchasedItem.objects.get(item_id=1)
-    po = PurchaseOrder.objects.get(buyer_id=2)
-
+    purchased_item = PurchasedItem.objects.get(item_id=1)
+    purchase_order = PurchaseOrder.objects.get(buyer_id=2)
     date= datetime.datetime.now()
-    bill_date=po.date_time
+    bill_date=purchase_order.date_time
     purchaseditem= PurchasedItem.objects.values('item','price','qty')
-    item = pi.item
-    price=pi.price
-    qty =pi.qty
-    sub_total= qty*price
-         
-    delivery_address=po.delivery_address
-    organisation=po.organisation
-    buyer_id=po.buyer_id
+    sub_total= purchased_item.qty * purchased_item.price        
+    delivery_address=purchase_order.delivery_address
+    organisation=purchase_order.organisation
+    buyer_id=purchase_order.buyer_id
     
-    return render(request, 'bill.html', { 'STC_No':'1', 'PAN_No' :'12', 'date': date, 'delivery_address' : delivery_address, 'Organisation' : organisation, 'buyer_id' : buyer_id, 'L_No.': '123', 'bill_date': bill_date, 'purchaseditem' : purchaseditem, 'item' : item, 'price' : price, 'qty':qty, 'total': sub_total} )
-
+    return render(request, 'bill.html', { 'STC_No':'1', 'PAN_No' :'12',
+    'date': date, 'delivery_address' : delivery_address, 
+    'Organisation' : organisation,'buyer_id' : buyer_id, 'L_No.': '123',
+    'bill_date': bill_date,'purchaseditem' : purchaseditem, 'total':sub_total})
