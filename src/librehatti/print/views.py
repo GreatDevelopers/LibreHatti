1c3c0f0202abc431bce0f43ea7569ebd7c2db662
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-06-26 23:33:53 +0530

    improvements

--- src/librehatti/print/views.py
+++ src/librehatti/print/views.py
@@ -7,12 +7,13 @@ from django.db.models import Sum
 
 def bill(request):
     
-    purchased_item = PurchasedItem.objects.get(item_id=1)
-    purchase_order = PurchaseOrder.objects.get(buyer_id=2)
-    date= datetime.datetime.now()
-    bill_date=purchase_order.date_time
+    purchased_item = PurchasedItem.objects.get(pk=1)
+    purchase_order = PurchaseOrder.objects.get(pk=2)
     purchaseditem= PurchasedItem.objects.values('item','price','qty')
-    sub_total= purchased_item.qty * purchased_item.price        
+    per_price = purchased_item.item.price
+    total=PurchasedItem.objects.filter(id=1).aggregate(Sum('price'))
+    date= datetime.datetime.now()
+    bill_date=purchase_order.date_time      
     delivery_address=purchase_order.delivery_address
     organisation=purchase_order.organisation
     buyer_id=purchase_order.buyer_id
@@ -20,4 +21,5 @@ def bill(request):
     return render(request, 'bill.html', { 'STC_No':'1', 'PAN_No' :'12',
     'date': date, 'delivery_address' : delivery_address, 
     'Organisation' : organisation,'buyer_id' : buyer_id, 'L_No.': '123',
-    'bill_date': bill_date,'purchaseditem' : purchaseditem, 'total':sub_total})
+    'bill_date': bill_date,'purchaseditem' : purchaseditem,'per_price': per_price, 'total_cost': total })
+
