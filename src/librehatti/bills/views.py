026f8b7e57d264b51003f71b3035495f54963b93
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2014-06-29 14:21:24 +0530

    Bills app and first view

---
+++ src/librehatti/bills/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from librehatti.catalog.models import *
+from librehatti.bills.models import *
+from django.contrib.auth.models import User
+import useraccounts
+
+
+def proforma(request):
+    QuotedOrder_list = QuotedOrder.objects.all()
+    QuotedItem_list = QuotedItem.objects.all()
+    return render(request, 'bills/quote.html', 
+                 {'QuotedOrder_list' : QuotedOrder_list})
