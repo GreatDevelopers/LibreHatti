8b31f0c37f695693363b56532a28f2356d96f685
divyanshikathuria <divyanshikathuria@gmail.com>
2014-06-30 21:50:03 +0530

    Delete urls.py

--- src/librehatti/bills/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, patterns 
-
-
-urlpatterns = patterns('librehatti.bills.views',
-        url(r'^proforma/', 'proforma') , 
-        url(r'^generateproforma/(?P<client_id>\d+)/', 'gen_proforma'),
-
-)
