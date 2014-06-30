5606361931a90f8936769d298957e2bef913c5aa
divyanshikathuria <divyanshikathuria@gmail.com>
2014-06-30 21:27:25 +0530

    Some small changes in the views and template

--- src/librehatti/print/views.py
+++ src/librehatti/print/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from librehatti.catalog.models import Category, PurchaseOrder,PurchasedItem
+from librehatti.catalog.models import Product, PurchaseOrder,PurchasedItem
 from django.db.models import Sum
 
 def add_material(request):
@@ -8,7 +8,7 @@ def add_material(request):
     start_date = request.POST['From']
     end_date = request.POST['To']
     purchase_item= PurchasedItem.objects.filter(purchase_order__date_time__range
-                  =(start_date,end_date)).filter(item__category__name=material).values_list( 
+                  =(start_date,end_date),item__name=material).values_list( 
                     'purchase_order_id','purchase_order__date_time',
                    'purchase_order__buyer_id__username',
                   'purchase_order__buyer_id__customer__title','price')
@@ -17,6 +17,6 @@ def add_material(request):
     return render(request, 'print/lab_reports.html', { 'purchase_item':purchase_item,
                  'start_date':start_date,'end_date':end_date,'total_cost':total})
   else:     
-    material_name = Category.objects.values('name')
+    material_name = Product.objects.values('name')
     return render(request,'print/form.html',{'material_name': material_name})    
     
