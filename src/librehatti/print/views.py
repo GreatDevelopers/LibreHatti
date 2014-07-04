47f2a73cfb17a5ccb3758e310e13cbac9689fa29
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-04 14:35:36 +0530

    final static view for bills

--- src/librehatti/print/views.py
+++ src/librehatti/print/views.py
@@ -5,11 +5,11 @@ from librehatti.catalog.models import *
 from django.db.models import Sum
 
 def bill(request):
-    purchase_order = PurchaseOrder.objects.get(buyer_id_id=5)
-    purchased_item = PurchasedItem.objects.filter(purchase_order_id=5).values(
+    purchase_order = PurchaseOrder.objects.get(buyer_id_id=1)
+    purchased_item = PurchasedItem.objects.filter(purchase_order_id=1).values(
     'item__name' ,'qty','item__price','price')	
-    total = PurchasedItem.objects.filter(purchase_order_id=5).aggregate(Sum('price')).get('price__sum', 0.00)
+    total = PurchasedItem.objects.filter(purchase_order_id=1).aggregate(Sum('price')).get('price__sum', 0.00)
     return render(request, 'bill.html', { 'STC_No' :'1','PAN_No' :'12', 'L_No': '123',
-     'purchase_order':purchase_order, 'purchased_item' : purchased_item,
-     'total_cost': total  })
+    'purchase_order':purchase_order, 'purchased_item' : purchased_item,
+    'total_cost': total  })
 
