e91d907429043d8062fdbd72ab694658a554a764
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-09 02:05:48 +0530

    final TA/DA  model

--- src/librehatti/suspence/models.py
+++ src/librehatti/suspence/models.py
@@ -3,16 +3,16 @@ from django.db import models
 # Create your models here.
 class TaDa(models.Model):
     #suspence = Model.ForeignKey(SuspenceOrder)
-    departure_time_from_tcc= Model.TimeField()
-    arrival_time_at_site = Model.TimeField()
-    departure_time_from_tcc = Model.TimeField()
-    arrival_time_at_tcc = Model.TimeField()
-    tada_amount = Model.IntegerField(initial=0)
-    start_test_date = Model.DateField()
-    end_test_date = Model.DateField()
-    testing_site= Model.CharField(max_length=100)
-    testing_staff = Model.CharField(max_length=100)
-    #def __unicode__(self):
+    departure_time_from_tcc= models.TimeField()
+    arrival_time_at_site = models.TimeField()
+    departure_time_from_tcc = models.TimeField()
+    arrival_time_at_site = models.TimeField()
+    tada_amount = models.IntegerField()
+    start_test_date = models.DateField()
+    end_test_date = models.DateField()
+    testing_site= models.CharField(max_length=100)
+    testing_staff = models.CharField(max_length=100)
+   # def __unicode__(self):
 	#	return self.suspence
 
 
