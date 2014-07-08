089475cdb22804ed1d820df712ddcf0c1aac4611
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-08 15:32:49 +0530

    models for Ta/Da

---
+++ src/librehatti/suspence/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class TaDa(models.Model):
+    #suspence = Model.ForeignKey(SuspenceOrder)
+    departure_time_from_tcc= Model.TimeField()
+    arrival_time_up = Model.TimeField()
+    departure_time_down = Model.TimeField()
+    arrival_time_down = Model.TimeField()
+    tada_amount = Model.IntegerField(initial=0)
+    start_test_date = Model.DateField()
+    end_test_date = Model.DateField()
+    testing_site= Model.CharField(max_length=100)
+    testing_staff = Model.CharField(max_length=100)
+     
+    
+    
