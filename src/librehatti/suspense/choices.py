62cde7f3f30802d69a79f6f7f4261c201f1f1f82
ashuS11 <ashumrsingh@gmail.com>
2014-07-12 00:55:10 +0530

    getting suspense ids from purchaseorder ids

---
+++ src/librehatti/suspense/choices.py
@@ -0,0 +1,11 @@
+from librehatti.catalog.models import *
+
+purchase_ids=[]
+a = PurchaseOrder.objects.all()
+	
+for i in a:
+	if i.is_suspense==True:
+		purchase_ids.append((i.id,i.id))
+suspense_ids=tuple(purchase_ids)
+CHOICES=suspense_ids
+	
