61aecc2802abb698bfec3615011ac5e84f4da6cc
Piyush <achyutapiyush@gmail.com>
2014-07-16 12:06:50 +0530

    Removing Unknown field error

--- src/librehatti/suspense/forms.py
+++ src/librehatti/suspense/forms.py
@@ -8,7 +8,7 @@ class Clearance_form(ModelForm):
     class Meta:
         model = SuspenseClearance
         fields = ['work_charge','labour_charge','car_taxi_charge','boring_charge_external','boring_charge_internal',
-		  'lab_testing_staff','field_testing_staff','Test_date']
+		  'lab_testing_staff','field_testing_staff','test_date']
 
 
 class SuspenseForm(forms.Form):
