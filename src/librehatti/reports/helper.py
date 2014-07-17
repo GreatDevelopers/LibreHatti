d75b4183ac8613dda95f234685729c7239a81dfc
GunpreetAhuja <gunpreetahuja0528@gmail.com>
2014-07-17 15:32:56 +0530

    reports' coding standards checked

--- src/librehatti/reports/helper.py
+++ src/librehatti/reports/helper.py
@@ -1,23 +1,26 @@
 import re
 from django.db.models import Q
 
+
 def normalize_query(query_string,
     findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
                     normspace=re.compile(r'\s{2,}').sub):
-    ''' Splits the query string in invidual keywords, getting rid of unecessary spaces
-        and grouping quoted words together.
+    ''' Splits the query string in invidual keywords, getting rid of 
+        unecessary spaces and grouping quoted words together.
         Example:
         
-        >>> normalize_query('  some random  words "with   quotes  " and   spaces')
-        ['some', 'random', 'words', 'with quotes', 'and', 'spaces']
-    
+        >>> normalize_query('  some random  words "with   quotes  " 
+            and   spaces')['some', 'random', 'words', 'with quotes', 
+            'and', 'spaces']
     '''
-    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(query_string)] 
+    return [normspace(' ', (t[0] or t[1]).strip()) for t in findterms(
+            query_string)] 
+
 
 def get_query(query_string, search_fields):
-    ''' Returns a query, that is a combination of Q objects. That combination
-        aims to search keywords within a model by testing the given search fields.
-    
+    ''' Returns a query, that is a combination of Q objects. That 
+        combination aims to search keywords within a model by testing 
+        the given search fields.
     '''
     query = None # Query to search for every search term        
     terms = normalize_query(query_string)
