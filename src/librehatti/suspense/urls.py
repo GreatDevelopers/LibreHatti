669325924eb613947a833ddce69d43874d6c9c22
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-18 23:00:27 +0530

    TA/DA work

--- src/librehatti/suspense/urls.py
+++ src/librehatti/suspense/urls.py
@@ -8,5 +8,9 @@ urlpatterns = patterns('librehatti.suspense.views',
                    url(r'^clresult/','clearance_result'),
                    url(r'^clsearch/','clearance_search'),
                    url(r'^othercharges/','other_charges'),
+                   url(r'^tada_form/', 'tada_form'),
+                   url(r'^tada_result/','tada_result'),
+                   url(r'^tada_search/','tada_search'),
+                   
               )                    
         
