2d9effb9b2afcd3f49660c22a06546a136e79bfc
aashitadutta <aashitadutta@gmail.com>
2014-07-19 01:21:09 +0530

    bills-views modified

--- src/librehatti/bills/views.py
+++ src/librehatti/bills/views.py
@@ -11,7 +11,7 @@ from librehatti.bills.forms import ConfirmForm
 def list_quoted(request):
     quoted = QuotedItem.objects.values('quote_order__id',
             'quote_order__quote_buyer_id__username').filter(confirm_status = 0)
-    return render(request,'bills/quoted_list.html',{'quoted':quoted}) 
+    return render(request,'bills/confform.html',{'quoted':quoted}) 
 
 
 def confirm(request):
@@ -30,7 +30,7 @@ def confirm(request):
         client_id = QuotedOrder.objects.values("quote_buyer_id__id").filter(
                     id = quote_order_id)
         form = ConfirmForm(initial={'quote_item':'item1', 'qty1':'quote_qty'})
-        return render(request, 'bills/confform.html', {'form':form,
+        return render(request, 'bills/bills.html', {'form':form,
                      'client_id':client_id, 'quoted_item' : quoted_item})
 
 
@@ -44,7 +44,7 @@ def gen_proforma(request, client_id):
     quoted_order=QuotedOrder.objects.get(quote_buyer_id_id=client_id)
     quoted_item = QuotedItem.objects.filter(quote_order_id=
                   client_id).values_list('quote_item__name',
-                'quote_item__category__name', 'quote_item__price', 'quote_qty',
+                'quote_item__category__name', 'quote_item__price_per_unit', 'quote_qty',
                 'quote_price')	
     total = QuotedItem.objects.filter(quote_order_id=client_id).aggregate(Sum(
             'quote_price')).get('quote_price__sum', 0.00)
