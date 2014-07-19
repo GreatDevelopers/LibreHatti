ee5a032af316e983b62bff6e19f3825608a42da6
arunbishnoi <arunbishnoi29@hotmail.com>
2014-07-19 17:44:40 +0530

    Tranport bill modified

--- src/librehatti/bills/urls.py
+++ src/librehatti/bills/urls.py
@@ -6,4 +6,7 @@ urlpatterns = patterns('librehatti.bills.views',
         url(r'^generateproforma/(?P<client_id>\d+)/', 'gen_proforma'),
 	url(r'^list_quoted', views.list_quoted, name ='list_quoted'),
        	url(r'^confirm/',views.confirm, name='confirm'),
+       	url(r'^transport/', 'transport'),
+        url(r'^transp/', 'transport_bill'),
+        #url(r'^add/', 'add_another'),
 )
--- src/librehatti/bills/views.py
+++ src/librehatti/bills/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from librehatti.catalog.models import PurchaseOrder
-from librehatti.catalog.models import PurchasedItem
-from librehatti.bills.models import QuotedOrder
-from librehatti.bills.models import QuotedItem
+from librehatti.catalog.models import PurchaseOrder, PurchasedItem
+from librehatti.bills.models import *
 from django.contrib.auth.models import User
 import useraccounts
 from django.db.models import Sum
-from librehatti.bills.forms import ConfirmForm
+from librehatti.bills.forms import *
+from librehatti.prints.helper import num2eng
+from django.db.models import Max
 
 
 def list_quoted(request):
@@ -52,4 +52,51 @@ def gen_proforma(request, client_id):
             'quote_price')).get('quote_price__sum', 0.00)
     return render(request, 'bills/p_bill.html',{ 'quoted_order':quoted_order,
                  'quoted_item' : quoted_item, 'total_cost': total })	 
+
+
+def transport(request):
+    form = transportform()
+    temp = {'transportform':form}
+    return render (request, 'bills/form.html',temp)
+
+
+def transport_bill(request):
+    if request.method == 'POST':
+        form = transportform(request.POST)
+        c = {}
+        if form.is_valid():
+            if 'button1' in request.POST:
+                    vehicle_id = request.POST['vehicle_id']
+                    job_id = request.POST['job_id']
+                    kilometer = request.POST['kilometer']          
+                    date = request.POST['date']
+                    rate = float(request.POST['rate'])
+                    total = rate*float(kilometer)
+                    obj = Transport(vehicle_id=vehicle_id, job_id=job_id, 
+                                    kilometer=kilometer, Date=date, rate=rate, 
+                                    total=total) 
+                    obj.save()
+                    temp = Transport.objects.filter(job_id=obj.job_id)
+                    total_amount = Transport.objects.filter(job_id=obj.job_id
+                           ).aggregate(Sum('total')).get('total__sum', 0.00)
+                    return render(request,'bills/transport_bill.html', 
+                           {'temp' : temp, 'words' : num2eng(int(total)), 
+                            'total_amount' : total_amount}) 
+            if 'button2' in request.POST:          
+                    form1 = transportform()
+                    vehicle_id = request.POST['vehicle_id']
+                    job_id = request.POST['job_id']
+                    kilometer = float(request.POST['kilometer'])
+                    date = request.POST['date']
+                    rate = float(request.POST['rate'])
+                    total = rate * kilometer
+                    obj = Transport(vehicle_id=vehicle_id, job_id=job_id, 
+                                    kilometer=kilometer, Date=date, rate=rate, 
+                                    total=total) 
+                    obj.save()
+                         
+    else:
+        form = transportform()
+    return render(request, 'bills/form.html', {'transportform':form})         
+  
   
