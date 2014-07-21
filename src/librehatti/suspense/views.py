ff02cf3ad04a378a26fb4e84d3536ab82b1a120b
Amitt Bhardwj <amitt.bhardwj@gmail.com>
2014-07-21 13:14:40 +0530

    Impelemented Coding Standard

--- src/librehatti/suspense/views.py
+++ src/librehatti/suspense/views.py
@@ -22,7 +22,8 @@ def clearance(request):
         ref_no = request.GET['q']
         cl_report = Clearance_form(initial = {'work_charge':0, 'labour_charge':
                     0, 'car_taxi_charge':0,'boring_charge_external':0,
-                    'boring_charge_internal':0,'Test_date':datetime.date.today})
+                    'boring_charge_internal':0,'Test_date':datetime.date.today
+                     })
         temp = {'q':ref_no,'cl_report':cl_report,}
         return render(request, 'suspense/suspenseform.html',temp)
 
@@ -38,8 +39,8 @@ def clearance_result(request):
         lab_testing_staff=request.GET['lab_testing_staff']
         field_testing_staff= request.GET['field_testing_staff']
         Test_date= request.GET['test_date']
-        obj= SuspenseClearance(suspense_id=ref_no, work_charge=work_charge, labour_charge=
-             labour_charge, car_taxi_charge=car_taxi_charge, 
+        obj= SuspenseClearance(suspense_id=ref_no, work_charge=work_charge,
+             labour_charge=labour_charge, car_taxi_charge=car_taxi_charge, 
              boring_charge_external=boring_charge_external,
              boring_charge_internal=boring_charge_internal,lab_testing_staff=
              lab_testing_staff,field_testing_staff=field_testing_staff,
@@ -55,7 +56,8 @@ def clearance_result(request):
 
 
 def other_charges(request):
-        obj = SuspenseClearance.objects.filter(id=1).values('boring_charge_external','labour_charge','car_taxi_charge')
+        obj = SuspenseClearance.objects.filter(id=1).values(
+              'boring_charge_external','labour_charge','car_taxi_charge')
         return render(request,'suspense/othercharge.html',{'obj':obj})
 
 
