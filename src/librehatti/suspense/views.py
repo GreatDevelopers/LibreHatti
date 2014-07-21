5224ec6691c158c61c26583cb261f8c38bf9094e
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-07-21 13:54:13 +0530

    standards checked

--- src/librehatti/suspense/views.py
+++ src/librehatti/suspense/views.py
@@ -91,13 +91,15 @@ def save_charges(request):
 	    
 def tada_search(request):
     form = TaDaSearch
-    return render( request, 'suspense/tada_search.html', { 'search_form': form })
+    return render( request, 'suspense/tada_search.html', {
+                    'search_form': form })
 
 
 def tada_form(request):
     ref_no = request.GET['ref_no']
     form= TaDaForm( initial = {'suspense': ref_no } )
-    return render( request, 'suspense/tada_form.html', { 'form' : form, 'ref_no':ref_no } ) 
+    return render( request, 'suspense/tada_form.html', {
+                        'form' : form, 'ref_no':ref_no } ) 
     
 
 
@@ -114,10 +116,13 @@ def tada_result(request):
        source_site= request.GET['source_site']
        testing_site= request.GET['testing_site']
        testing_staff= request.GET['testing_staff']
-       obj= TaDa(suspense=suspense, departure_time_from_tcc =departure_time_from_tcc ,arrival_time_at_site=arrival_time_at_site,
-       departure_time_from_site=departure_time_from_site,arrival_time_at_tcc=arrival_time_at_tcc,
-       tada_amount=tada_amount,start_test_date=start_test_date,end_test_date=end_test_date, source_site=source_site,
-       testing_site=testing_site,testing_staff=testing_staff)
+       obj= TaDa(suspense=suspense, departure_time_from_tcc =
+            departure_time_from_tcc,arrival_time_at_site=arrival_time_at_site,
+            departure_time_from_site=departure_time_from_site,
+            arrival_time_at_tcc=arrival_time_at_tcc,tada_amount=tada_amount,
+            start_test_date=start_test_date,end_test_date=end_test_date, 
+            source_site=source_site, testing_site=testing_site,
+            testing_staff=testing_staff )
        obj.save()
        i=TaDa.objects.all().aggregate(Max('id'))
        j=i['id__max']
@@ -127,9 +132,10 @@ def tada_result(request):
          total_cost= total_cost + int(tada_amount)
        rupees_in_words = num2eng(total_cost)
        obj1 = TaDa.objects.filter(id=j).values('departure_time_from_tcc' ,
-       'arrival_time_at_site','departure_time_from_site','arrival_time_at_tcc', 
-       'tada_amount','start_test_date','end_test_date','source_site', 'testing_site','testing_staff')
-       #total_cost = TaDa.objects.filter(id=j).aggregate(Sum('tada_amount')).get('tada_amount__sum', 0.00)
+              'arrival_time_at_site','departure_time_from_site',
+              'arrival_time_at_tcc', 'tada_amount','start_test_date',
+              'end_test_date','source_site', 'testing_site','testing_staff')
        return render(request, 'suspense/tada_result.html', { 'obj':obj1, 
-       'total_cost':total_cost, 'staff':staff, 'rupees_in_words':rupees_in_words })
+       'total_cost':total_cost, 'staff':staff, 'rupees_in_words':
+       rupees_in_words })
 
