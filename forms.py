d160398836614efb07b007dea455b7fac18b6f67
sanjeetkaur <saify171994@gmail.com>
2014-07-22 23:43:46 +0530

    updated forms.py

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from librehatti.catalog.models import Category
 
 class AddCategory(forms.Form):
     """
-    It is used by the views of catalog to specify the name of category and different categories used. 
+    used by catalog views to specify categories used. 
     """
     category_name = forms.CharField(max_length=256)
     categories = forms.ModelChoiceField(queryset=Category.objects.all())
