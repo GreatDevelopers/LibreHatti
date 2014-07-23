e9e157ddc9975d80c2929706e403b9a66984d160
Muskana <muskan.ar912@gmail.com>
2014-07-23 17:29:26 +0530

    documentation of urls.py

---
+++ src/librehatti/suspense/urls_html_output/search/all_64.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['department',['Department',['../classlibrehatti_1_1suspense_1_1models_1_1Department.html',1,'librehatti::suspense::models']]]
+];
---
+++ src/librehatti/suspense/urls_html_output/search/classes_6d.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['meta',['Meta',['../classlibrehatti_1_1suspense_1_1forms_1_1Clearance__form_1_1Meta.html',1,'librehatti::suspense::forms::Clearance_form']]],
+  ['meta',['Meta',['../classlibrehatti_1_1suspense_1_1forms_1_1TaDaForm_1_1Meta.html',1,'librehatti::suspense::forms::TaDaForm']]]
+];
