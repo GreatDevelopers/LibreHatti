59bb9e06a139447df5dccc7ef96e6526935fda4f
Jasvir Singh <jassigrewal91@gmail.com>
2014-08-15 15:12:32 +0530

    Enabling django docs. We need python docutils for it.

--- src/librehatti/settings.py
+++ src/librehatti/settings.py
@@ -80,6 +80,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admindocs',
     'django.contrib.admin',
     'librehatti.catalog',
     'useraccounts',
