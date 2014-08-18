0f14891f6f782397fc6888bacd3d0e678d7db362
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-08-18 13:47:28 +0530

    presently not needed constraints commented

--- src/librehatti/reports/search_choices.py
+++ src/librehatti/reports/search_choices.py
@@ -1,15 +1,14 @@
 CLIENT_FIELD_CHOICES = (
-	('name', 'Name'), ('city', 'Address'), ('phone', 'Phone'), 
-	('company', 'Company'),
-	)
+	             ('name', 'Name'), ('city', 'Address'), 
+	                ('phone', 'Phone'), ('company', 'Company'),
+	              )
 	  
-CLIENT_ORDER_CHOICES=(
-   
-    ('discount', 'Discount'), ('debit', 'Debit'), 
-    ('mode of payment','Payment Mode'),
-	)
-
-		
-CONSTRAINT_CHOICES = (
-    ('date', 'Date'), ('gt', 'Amount greater than'), ('lt', 'Amount lesser than'),
-	)
+CLIENT_ORDER_CHOICES= (
+            ('discount', 'Discount'), ('debit', 'Debit'), 
+                ('mode of payment','Payment Mode'),
+	          )
+	          
+CONSTRAINT_CHOICES= (
+                      ('date', 'Date'),#('gt', 'Amount greater than'),	
+                      #('lt', 'Amount lesser than'),
+                      )
