11d784eca57b825a4cbca72daf710f07ee66e4f6
saloni <saloni@saloni-Lenovo-B560.(none)>
2014-08-18 13:04:08 +0530

    constraint class removed

--- src/librehatti/reports/forms.py
+++ src/librehatti/reports/forms.py
@@ -1,8 +1,7 @@
 from django import forms
+
 from search_choices import CLIENT_FIELD_CHOICES
 from search_choices import CLIENT_ORDER_CHOICES
-from search_choices import CONSTRAINT_CHOICES
-import datetime
 
 
 """
@@ -19,15 +18,3 @@ displays chechboxes for Order Search
 class OrderForm(forms.Form):
     order = forms.MultipleChoiceField(required=False,
     widget=forms.CheckboxSelectMultiple, choices=CLIENT_ORDER_CHOICES)
-
-
-"""
-displays checkboxes for Constraints
-"""
-class AddConstraints(forms.Form):
-    start_date = forms.DateField(required=False, initial='2014-01-01')
-    end_date = forms.DateField(required=False, initial= datetime.date.today())
-    additional_constraints = forms.MultipleChoiceField(required=False,
-    widget=forms.CheckboxSelectMultiple, choices= CONSTRAINT_CHOICES)
-    amount_greater_than = forms.FloatField(required=False, initial = 0)
-    amount_less_than = forms.FloatField(required=False, initial = 1000000)
