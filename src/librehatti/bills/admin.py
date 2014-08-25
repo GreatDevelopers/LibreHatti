5ec0fb5acdba8a76b490842d53d4b308ea035f10
aashitadutta <aashitadutta@gmail.com>
2014-08-25 18:53:50 +0530

    admin.py changed

--- src/librehatti/bills/admin.py
+++ src/librehatti/bills/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from librehatti.bills.models import QuotedItem
-from librehatti.bills.models import QuotedOrder
+from librehatti.bills.models import *
+from django.contrib.auth.admin import *
+from librehatti.catalog.actions import mark_inactive, mark_active
+admin.autodiscover() 
 
 
 class QuotedItemInline(admin.StackedInline):
@@ -10,7 +12,16 @@ class QuotedItemInline(admin.StackedInline):
 
 
 class QuotedOrderAdmin(admin.ModelAdmin):
+    exclude=('is_active',)
+    list_display = ['id','quote_buyer','quote_delivery_address',
+                    'quote_date_time','is_active']
     inlines = [QuotedItemInline]
+    model = QuotedOrder
+    actions = [mark_active, mark_inactive] 
+    list_filter = ['quote_date_time']
+    search_fields = ['id']
+    list_per_page = 20 
+   
 
 
 admin.site.register(QuotedOrder, QuotedOrderAdmin)
