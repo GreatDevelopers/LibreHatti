8a6cd307e554726f21c0b419cb3838580af61ee7
ashuS11 <ashumrsingh@gmail.com>
2014-08-31 21:02:12 +0530

    redirection to table with order details

--- src/librehatti/bills/admin.py
+++ src/librehatti/bills/admin.py
@@ -1,26 +1,35 @@
 from django.contrib import admin
 from librehatti.bills.models import *
 from django.contrib.auth.admin import *
+from librehatti.bills.forms import ItemSelectForm
 from librehatti.catalog.actions import mark_inactive, mark_active
+from django.http import HttpResponse,HttpResponseRedirect
 admin.autodiscover() 
 
 
 class QuotedItemInline(admin.StackedInline):
     model = QuotedItem
-    fields = ['quote_item', 'quote_qty', 'quote_discount']
+    form = ItemSelectForm
+    fields = ['parent_category', 'sub_category','item','qty']
     extra = 10
 
 
 class QuotedOrderAdmin(admin.ModelAdmin):
     exclude=('is_active',)
-    list_display = ['id','quote_buyer_id','quote_delivery_address',
-                    'quote_date_time','is_active']
+    list_display = ['id','buyer_id','delivery_address','organisation',
+                    'total_discount','tds','mode_of_payment','confirm_status']
     inlines = [QuotedItemInline]
     model = QuotedOrder
     actions = [mark_active, mark_inactive] 
-    list_filter = ['quote_date_time']
+    list_filter = ['date_time']
     search_fields = ['id']
     list_per_page = 20 
+    def response_add(self, request, obj, post_url_continue=None):
+        request.session['old_post'] = request.POST
+        request.session['quote_order_id'] = obj.id
+	return HttpResponseRedirect('/bills/bill_cal')
+        
+
    
 
 admin.site.register(QuotedOrder, QuotedOrderAdmin)
