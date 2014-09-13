ac1c16e82d486fa5896abbd30360361419e75d43
Jasvir Singh <jassigrewal91@gmail.com>
2014-09-13 19:34:07 +0530

    Resgisters extended. More constraints added.

--- src/librehatti/reports/forms.py
+++ src/librehatti/reports/forms.py
@@ -3,7 +3,14 @@ from django import forms
 from search_choices import CLIENT_FIELD_CHOICES
 from search_choices import CLIENT_ORDER_CHOICES
 from search_choices import CONSTRAINT_CHOICES
+from search_choices import MONTH_CHOICES
+
 import datetime
+import itertools
+
+from librehatti.voucher.models import FinancialSession
+from librehatti.catalog.models import ModeOfPayment
+from librehatti.catalog.models import Surcharge
 
 """
 displays checkboxes for Client Search
@@ -24,9 +31,43 @@ class OrderForm(forms.Form):
 displays checkboxes for Constraints
 """
 class AddConstraints(forms.Form):
-    start_date = forms.DateField(required=False, initial='2014-01-01')
-    end_date = forms.DateField(required=False, initial= datetime.date.today())
+    
     additional_constraints = forms.MultipleChoiceField(required=False,
     widget=forms.CheckboxSelectMultiple, choices= CONSTRAINT_CHOICES)
-    #amount_greater_than = forms.FloatField(required=False, initial = 0)
-    #amount_less_than = forms.FloatField(required=False, initial = 1000000)
+    
+    amount_greater_than = forms.FloatField(required=False, initial = 0)
+    amount_less_than = forms.FloatField(required=False, initial = 1000000)
+
+    start_date = forms.DateField(required=False, initial=str(datetime.date.\
+        today().year) + '-04-01')
+    end_date = forms.DateField(required=False, initial= datetime.date.today())
+    year = forms.ChoiceField(choices= [(datetime.date.today().year, datetime.\
+        date.today().year)] + [(x, x) for x in range(2000, 2050)], \
+        required=False)
+    month = forms.ChoiceField(MONTH_CHOICES)
+    
+    session_id = FinancialSession.objects.values_list('id',flat = True)
+    session_start = FinancialSession.objects.values_list('session_start_date',\
+        flat = True)
+    session_end = FinancialSession.objects.values_list('session_end_date', \
+        flat = True)
+    session_choices = [('', '--------')] + [(id, str(start) + '-To-' + \
+        str(end)) for id, start, end in itertools.izip(session_id, \
+        session_start, session_end)]
+    session = forms.ChoiceField(session_choices)
+    
+    surcharges = forms.ModelMultipleChoiceField(required=False,
+    widget=forms.CheckboxSelectMultiple, queryset= Surcharge.objects.filter(
+        taxes_included = True))
+    paid_surcharges = forms.BooleanField()
+
+    mode_of_payment_id = ModeOfPayment.objects.values_list('id', flat = True)
+    mode = ModeOfPayment.objects.values_list('method', flat = True)
+    mode_choices = [('', '--------')] + [(id, mode) for id, mode in itertools.\
+        izip(mode_of_payment_id, mode)]
+    mode_of_payment = forms.ChoiceField(mode_choices)
+
+    grand_total = forms.BooleanField()
+    all_registered_user = forms.BooleanField()
+
+
