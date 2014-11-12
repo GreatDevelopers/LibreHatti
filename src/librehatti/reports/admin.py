3c62af5995550f703d314db4e12599fae146b87f
Jasvir Singh <jassigrewal91@gmail.com>
2014-11-12 13:04:42 +0530

    adding suit templates and updating top branding link.

---
+++ src/librehatti/reports/admin.py
@@ -0,0 +1,8 @@
+
+from django.contrib import admin
+from django.contrib.auth.admin import *
+from librehatti.reports.models import SavedRegisters
+
+admin.autodiscover()
+
+admin.site.register(SavedRegisters)
