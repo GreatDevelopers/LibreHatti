835cca355e0e4835db40cb7cedb33331cb044ded
Aseem <mittalaseem79@gmail.com>
2014-11-26 17:31:55 +0530

    Views created for note add on quoto order and proforma bill updated for note selected

--- src/librehatti/bills/views.py
+++ src/librehatti/bills/views.py
@@ -7,19 +7,24 @@ from librehatti.catalog.models import PurchasedItem
 from librehatti.catalog.models import ModeOfPayment
 from librehatti.catalog.models import Product
 from librehatti.catalog.models import HeaderFooter
+from librehatti.catalog.models import Surcharge
 from librehatti.bills.models import QuotedTaxesApplied
 from librehatti.bills.models import QuotedOrder
+from librehatti.bills.models import QuotedBill
 from librehatti.bills.models import QuotedItem
+from librehatti.bills.models import QuotedOrderNote
+from librehatti.bills.models import NoteLine
 from librehatti.suspense.models import QuotedSuspenseOrder
 from django.contrib.auth.models import User
 import useraccounts
 from django.db.models import Sum
-from librehatti.bills.forms import *
+from librehatti.bills.forms import SelectNoteForm
 from django.db.models import Max
 import simplejson
 from django.contrib.auth.decorators import login_required
 from librehatti.bills.forms import ItemSelectForm
 from django.core.urlresolvers import reverse
+from librehatti.catalog.request_change import request_notify
 
 """
 This view calculate taxes on quoted order, bill data
@@ -68,7 +73,7 @@ def quoted_bill_cal(request):
     bill.save()
     request.session['old_post'] = old_post
     request.session['quoted_order_id'] = quoted_order_id
-    return HttpResponseRedirect(reverse("librehatti.bills.views.quoted_order_added_success"))
+    return HttpResponseRedirect(reverse("librehatti.bills.views.select_note"))
 
 
 @login_required
@@ -80,3 +85,55 @@ def quoted_order_added_success(request):
         'cheque_dd_number','cheque_dd_date').filter(id=quoted_order_id)[0]
     return render(request,'bills/quoted_success.html',{'details': details,
         'quoted_order_id':quoted_order_id})
+
+
+@login_required
+def select_note(request):
+    quoted_order_id = request.session.get('quoted_order_id')
+    form = SelectNoteForm(initial={'quoted_order':quoted_order_id})
+    request_status = request_notify()
+    return render(request, 'bills/select_note.html', \
+        {'form':form,'request':request_status})
+
+
+@login_required
+def select_note_save(request):
+    if request.method == 'POST':
+        form = SelectNoteForm(request.POST)
+        if form.is_valid():
+            formdata = form.cleaned_data
+            quoted_order = formdata['quoted_order']
+            quoted_order_id = QuotedOrder.objects.get(id=quoted_order)
+            note_list=[]
+            for note in formdata['note_line']:
+                note_list.append(note)
+            for value in note_list:
+                obj=QuotedOrderNote(quoted_order=quoted_order_id,note=value)
+                obj.save()
+
+            return HttpResponseRedirect(\
+                reverse("librehatti.bills.views.quoted_order_added_success"))
+        else:
+            return HttpResponseRedirect(\
+                reverse("librehatti.bills.views.quoted_order_added_success"))
+    else:
+        error_type = "404 Forbidden"
+        error = "Please again place the order"
+        temp = {'type': error_type, 'message':error}
+        return render(request, 'error_page.html', temp)
+
+
+@login_required
+def new_note_line(request):
+    note_line = request.GET['note_line']
+    obj = NoteLine(note=note_line)
+    obj.save()
+    return HttpResponse('')
+
+@login_required
+def delete_note(request):
+    delete_note = request.GET['delete_note']
+    delete_note_id = delete_note.split(',')
+    for id in delete_note_id:
+        NoteLine.objects.filter(id=id).delete()
+    return HttpResponse('')
