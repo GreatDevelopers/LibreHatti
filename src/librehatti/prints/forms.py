779726a17fe96e397b560dafdd1b52fcbc632b69
Aseem <mittalaseem79@gmail.com>
2014-12-03 19:24:59 +0530

    Coding guidelines followed in prints

--- src/librehatti/prints/forms.py
+++ src/librehatti/prints/forms.py
@@ -1,9 +1,11 @@
 from django import forms
 from librehatti.catalog.models import Category
+
 import itertools
 
 from librehatti.config import _PARENT_CATEGORY
 from librehatti.config import _SUB_CATEGORY
+
 
 """
 This form lets the user select the category to generate the lab report.
@@ -12,12 +14,14 @@ class LabReportForm(forms.Form):
     start_date = forms.DateField()
     end_date = forms.DateField()
     parent_category = forms.ModelChoiceField(queryset=Category.objects.\
-    filter(parent__parent__isnull=True).filter(parent__isnull=False), label = _PARENT_CATEGORY)
+    filter(parent__parent__isnull=True).filter(parent__isnull=False),\
+    label=_PARENT_CATEGORY)
     try:
-        sub_category_id = Category.objects.values_list('id',flat=True)
-        sub_category_name = Category.objects.values_list('name',flat=True)
-        sub_category_choices = [('', '--------')] + [(id, name) for id, name in itertools.\
-        izip(sub_category_id, sub_category_name)]
-        sub_category = forms.ChoiceField(sub_category_choices, label = _SUB_CATEGORY)
+        sub_category_id = Category.objects.values_list('id', flat=True)
+        sub_category_name = Category.objects.values_list('name', flat=True)
+        sub_category_choices = [('', '--------')] + [(id, name) for id,\
+        name in itertools.izip(sub_category_id, sub_category_name)]
+        sub_category = forms.ChoiceField(sub_category_choices,\
+            label=_SUB_CATEGORY)
     except:
         pass
