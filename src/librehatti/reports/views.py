75fe8bed229233165555cba62d5c3fc9f5ea1fcc
haramritpanesar <haramritpanesar@gmail.com>
2014-12-13 13:45:35 +0530

    Daily Report Register updated

--- src/librehatti/reports/views.py
+++ src/librehatti/reports/views.py
@@ -95,40 +95,46 @@ def list_saved_registers(request):
         'request':request_status})
 
 
-def daily_report_form(request):
-    """
-    This view is used to render the form for daily report registers
-    """
-    form = DailyReportForm()
-    request_status = request_notify()
-    return render(request,'reports/daily_report_form.html', \
-        {'form':form,'request':request_status})
+# def daily_report_form(request):
+#     """
+#     This view is used to render the form for daily report registers
+#     """
+#     form = DailyReportForm()
+#     request_status = request_notify()
+#     return render(request,'reports/daily_report_form.html', \
+#         {'form':form,'request':request_status})
 
 
 @login_required
 def daily_report_result(request):
     """
     This view is used to display the daily report registers
-    """
-
-    if 'button1' in request.POST:
-        form = DailyReportForm(request.POST)
-        if form.is_valid():
-            start_date = request.POST['start_date']
-            end_date = request.POST['end_date']
-            mode_of_payment = request.POST['Type']
-            list_of_report = []
-            purchase_order = PurchaseOrder.objects.filter(mode_of_payment = mode_of_payment).values('date_time','id')
-            for date_value in purchase_order:
-                if  start_date <= str(date_value['date_time']) <= end_date:
-                    bill_object = Bill.objects.filter(purchase_order_id = date_value['id']).values('grand_total','purchase_order_id','purchase_order__date_time','purchase_order__buyer__first_name','purchase_order__buyer__last_name','purchase_order__buyer__customer__user__customer__address__street_address','purchase_order__buyer__customer__user__customer__address__city')
-                    list_of_report.append(bill_object)
-                else:
-                    pass
-            sum = 0
-            for temp_var in list_of_report:
-                for total in temp_var:
-                    sum = sum + total['grand_total']
-    request_status = request_notify()
-    return render(request,'reports/daily_report_result.html',\
-        {'list_of_report':list_of_report,'sum':sum,'request':request_status})
+    """ 
+    if request.method == 'POST':
+        if 'button1' in request.POST:
+            form = DailyReportForm(request.POST)
+            if form.is_valid():
+                start_date = request.POST['start_date']
+                end_date = request.POST['end_date']
+                mode_of_payment = request.POST['Type']
+                list_of_report = []
+                purchase_order = PurchaseOrder.objects.filter(mode_of_payment = mode_of_payment).values('date_time','id')
+                for date_value in purchase_order:
+                    if  start_date <= str(date_value['date_time']) <= end_date:
+                        bill_object = Bill.objects.filter(purchase_order_id = date_value['id']).values('grand_total','purchase_order_id','purchase_order__date_time','purchase_order__buyer__first_name','purchase_order__buyer__last_name','purchase_order__buyer__customer__user__customer__address__street_address','purchase_order__buyer__customer__user__customer__address__city')
+                        list_of_report.append(bill_object)
+                    else:
+                        pass
+                sum = 0
+                for temp_var in list_of_report:
+                    for total in temp_var:
+                        sum = sum + total['grand_total']
+        request_status = request_notify()
+        return render(request,'reports/daily_report_result.html',\
+            {'list_of_report':list_of_report,'sum':sum,'request':request_status})
+    
+    else:
+        form = DailyReportForm()
+        request_status = request_notify()
+        return render(request,'reports/daily_report_form.html', \
+        {'form':form,'request':request_status})
