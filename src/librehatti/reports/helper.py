72c1558cab63db4168493592677230aba6de3de3
Manpreet Dhiman <manpreet9112@gmail.com>
2015-10-28 20:52:54 +0530

    Update helper.py

--- src/librehatti/reports/helper.py
+++ src/librehatti/reports/helper.py
@@ -7,7 +7,7 @@ def normalize_query(query_string,
     findterms=re.compile(r'"([^"]+)"|(\S+)').findall,
                     normspace=re.compile(r'\s{2,}').sub):
     ''' Splits the query string in invidual keywords, getting rid of
-        unecessary spaces and grouping quoted words together.
+        unnecessary spaces and grouping quoted words together.
         Example:
 
         >>> normalize_query('  some random  words "with   quotes  "
