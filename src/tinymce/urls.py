4e07f9b7c20a18c7b073a7ccbb3685b86933f407
amisha2016 <amishabudhiraja96@gmail.com>
2017-12-08 00:44:39 +0530

    Fixed NoReverse Match Error at Catalog>Header Footer

--- src/tinymce/urls.py
+++ src/tinymce/urls.py
@@ -10,8 +10,8 @@ from . import views
 urlpatterns = [
     url(r'^js/textareas/(?P<name>.+)/$', views.textareas_js, name='tinymce-js'),
     url(r'^js/textareas/(?P<name>.+)/(?P<lang>.*)$', views.textareas_js, name='tinymce-js-lang'),
-    url(r'^spellchecker/$', views.spell_check),
-    url(r'^flatpages_link_list/$', views.flatpages_link_list),
+    url(r'^spellchecker/$', views.spell_check, name='spell_check'),
+    url(r'^flatpages_link_list/$', views.flatpages_link_list, name='flatpages_link_list'),
     url(r'^compressor/$', views.compressor, name='tinymce-compressor'),
     url(r'^filebrowser/$', views.filebrowser, name='tinymce-filebrowser'),
     url(r'^preview/(?P<name>.+)/$', views.preview, name='tinymce-preview'),
